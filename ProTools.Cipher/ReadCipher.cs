a2ca28ebcbe2880aba229f089259fec15d04faec
Mina Ehab <42187982+minaeh@users.noreply.github.com>
2024-06-21 22:06:28 +0300

    Intial

---
+++ ProTools.Cipher/ReadCipher.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace ProTools.Cipher
+{
+    public static class ReadCipher
+    {
+        public static string ReadEnecryptFile(string fullPath, string cipherKey,out string encryptContent)
+        {
+            if (fullPath == null)
+            {
+                throw new ArgumentNullException(nameof(fullPath), "File path is null");
+            }
+            if (File.Exists(fullPath))
+            {
+                encryptContent = File.ReadAllText(fullPath);
+                return SymmetricKey.DecryptString(cipherKey, encryptContent);
+            }
+            else
+            {
+                throw new FileNotFoundException($"Cannot find file at '{fullPath}'");
+            }
+        }
+
+        public static T ReadEnecryptFile<T>(string fullPath, string cipherKey) where T : class, new()
+        {
+            string decryptcontetn = ReadEnecryptFile(fullPath, cipherKey, out string encryptContent);
+            return JsonConvert.DeserializeObject<T>(decryptcontetn);
+        }
+    }
+}
